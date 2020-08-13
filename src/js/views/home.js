be0aeda5591ef37f7903c24f57b256ad58b59dd4
Laura-Romero <laura90romero@gmail.com>
2020-08-13 08:22:45 +0000

    details

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "../../styles/home.scss";
 import { Header } from "../component/header";
+import { CardsDetails } from "../component/cardsDetails";
+
 export const Home = () => {
 	return (
-		<div className="container-fluid header">
-			<div className="row">
+		<div className="container-fluid ">
+			<div className="row header ">
 				<Header />
+			</div>
+			<div className="row">
+				<CardsDetails />
 			</div>
 		</div>
 	);
