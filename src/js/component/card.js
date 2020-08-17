19c98c03020c531f1b01c15c09818cde39ef222c
dilbert-gus <g-u-s1994@hotmail.com>
2020-08-17 17:15:25 +0000

    actualizacion ficheros

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,18 +1,39 @@
 import React from "react";
 import { Fragment } from "react";
+
 import "../../styles/home.scss";
 
 export const Card = () => {
 	return (
 		<Fragment>
-			<div className="card">
+			<div className="card mx-5">
 				<img src="..." className="cardtop" alt="..." />
 				<div className="card-body">
 					<h5 className="card-title">{"LUKE SKYWALKER"}</h5>
-					<p className="card-text">{"Gender: male Hair Color: blonde Eye-Color:blue"}</p>
+					<p className="card-text">{"Gender: male Hair Color:blonde Eye-Color:blue"}</p>
 				</div>
-				<button className="learnMore">Learn more!</button>
-				<i className="fa fa-heart" aria-hidden="true" />
+				<div className="d-flex justify-content-between">
+					<button className="learnMore">Learn more!</button>
+					<i className="fa fa-heart" aria-hidden="true" />
+				</div>
+			</div>
+		</Fragment>
+	);
+};
+
+export const Planets = () => {
+	return (
+		<Fragment>
+			<div className="Planets">
+				<img src="" className="cardbox" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">{"Alderaan"}</h5>
+					<p className="card-text">{"Population: 2000000000 Terrain: grassiands, mountains"}</p>
+				</div>
+				<div className="d-flex justify-content-between">
+					<button className="learnMore1">Learn more!</button>
+					<i className="fa fa-heart" aria-hidden="true" />
+				</div>
 			</div>
 		</Fragment>
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,7 +2,30 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import { Card } from "../component/card.js";
+import { Planets } from "../component/card.js";
 
 export const Home = () => {
-	return <Card />;
+	return (
+		<div>
+			<h1>Characters</h1>
+			<div className="d-flex cardContainer">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+			<h1>planets</h1>
+			<div className="d-flex cardContainer">
+				<Planets />
+				<Planets />
+				<Planets />
+				<Planets />
+				<Planets />
+				<Planets />
+			</div>
+			<div />
+		</div>
+	);
 };
